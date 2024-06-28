e088716c71b072bf551b6aaa6ae758db75525b3b
Macondiana <22807958+dcarorod@users.noreply.github.com>
2024-06-28 10:20:33 +0200

    ft_strdup added

---
+++ rank_02/level_2/ft_strdup/ft_strdup.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaro-ro <dcaro-ro@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 10:05:17 by dcaro-ro          #+#    #+#             */
+/*   Updated: 2024/06/28 10:09:34 by dcaro-ro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (*str)
+		len++;
+	return (len);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = ft_strlen(src);
+	dest = malloc((sizeof(char) * i) + 1);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
