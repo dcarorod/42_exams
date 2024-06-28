4e4a3d4b954f2a7a30f663c1e4bf740ce53ee394
Macondiana <22807958+dcarorod@users.noreply.github.com>
2024-06-28 10:49:17 +0200

    ft_strrev added

---
+++ rank_02/level_2/ft_strrev/ft_strrev.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaro-ro <dcaro-ro@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 10:41:54 by dcaro-ro          #+#    #+#             */
+/*   Updated: 2024/06/28 10:49:02 by dcaro-ro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strrev(char *str)
+{
+	int		i;
+	int		len;
+	char	temp;
+
+	len = ft_strlen(str);
+	len--;
+	i = 0;
+	while (len > i)
+	{
+		temp = str[i];
+		str[i] = str[len];
+		str[len] = temp;
+		i++;
+		len--;
+	}
+	return (str);
+}
