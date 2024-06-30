ca0b4cbd8352171f0c1afa30afa53f31858e1b26
Diana Caro <dcaro-ro@student.42.fr>
2024-06-30 17:41:30 +0200

    ft_split added

---
+++ rank_02/level_4/ft_split/ft_split.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaro-ro <dcaro-ro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/30 16:25:13 by dcaro-ro          #+#    #+#             */
+/*   Updated: 2024/06/30 17:40:10 by dcaro-ro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int	ft_isspace(char c)
+{
+	return ((c >= 9 && c <= 13) || c == 32);
+}
+
+static size_t	count_words(char const *str)
+{
+	int		in_word;
+	size_t	wc;
+	int		i;
+
+	in_word = 0;
+	wc = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (ft_isspace(str[i]))
+		{
+			if (in_word == 1)
+				in_word = 0;
+		}
+		else if (in_word == 0)
+		{
+			wc++;
+			in_word = 1;
+		}
+		i++;
+	}
+	return (wc);
+}
+
+static	size_t	ft_wordlen(char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len] && !ft_isspace(str[len]))
+		len++;
+	return (len);
+}
+
+static char	*ft_strncpy(char *dest, const char *src, int n)
+{
+	int	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	**ft_split(char *str)
+{
+	int		i = 0;
+	size_t	wlen;
+	char	**dest;
+	int		wc = count_words(str);
+
+	dest = malloc(sizeof(char *) * (wc + 1));
+	if (!dest)
+		return (NULL);
+	while (*str)
+	{
+		while (*str && ft_isspace(*str))
+			str++;
+		if (*str && !ft_isspace(*str))
+		{
+			wlen = ft_wordlen(str);
+			dest[i] = malloc(sizeof(char) * (wlen + 1));
+			if (!dest[i])
+			{
+				while (i > 0)
+					free(str[--i]);
+				free(dest);
+				return (NULL);
+			}
+			ft_strncpy(dest[i], str, wlen);
+			i++;
+		}
+		str += wlen;
+	}
+	dest[i] = NULL;
+	return (dest);
+}
+
+
+
+/*#include <stdio.h>
+int	main(void)
+{
+	char *str = " djf ";
+	int	counter = count_words(str);
+	int	word_len = ft_wordlen(str);
+
+	printf("String: %s\n", str);
+	printf("Word count: %d\n", counter);
+	printf("Word length: %d\n", word_len);
+	return (0);
+}*/
