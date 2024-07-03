bc62010d46a95eeebd0d1ab42b327996b46ca1ea
Diana Caro <dcaro-ro@student.42.fr>
2024-07-03 13:27:21 +0200

    sort_list updated and tested

--- rank_02/level_4/sort_list/sort_list.c
+++ rank_02/level_4/sort_list/sort_list.c
@@ -6,13 +6,13 @@
 /*   By: dcaro-ro <dcaro-ro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:56:25 by dcaro-ro          #+#    #+#             */
-/*   Updated: 2024/07/02 17:13:24 by dcaro-ro         ###   ########.fr       */
+/*   Updated: 2024/07/03 13:26:56 by dcaro-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "list.h"
 
-static void	ft_swap(t_list *a, t_list *b)
+static	void	ft_swap(t_list *a, t_list *b)
 {
 	int	temp;
 
@@ -24,15 +24,16 @@ static void	ft_swap(t_list *a, t_list *b)
 t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 {
 	t_list	*current;
-	int		swapped = 1;
+	t_list	*last_sorted;
+	int	swapped;
 
-	if (!lst)
-		return (0);
-	current = lst;
+	last_sorted = 0;
+	swapped = 1;
 	while (swapped)
 	{
 		swapped = 0;
-		while (current != 0 && current->next != 0)
+		current = lst;
+		while (current->next != last_sorted)
 		{
 			if (cmp(current->data, current->next->data) == 0)
 			{
@@ -41,7 +42,74 @@ t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 			}
 			current = current->next;
 		}
-		current = lst;
+		last_sorted = current;
 	}
 	return (lst);
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+static int	ascending(int a, int b)
+{
+	return (a <= b);
+}
+
+static t_list	*create_node(int data)
+{
+	t_list	*new_node = malloc(sizeof(t_list));
+
+	if (!new_node)
+		return (0);
+	new_node->data = data;
+	new_node->next = 0;
+	return (new_node);
+}
+
+static void	print_list(t_list *lst)
+{
+	while (lst)
+	{
+		printf("%d ", lst->data);
+		lst = lst->next;
+	}
+}
+
+static void	free_list(t_list *lst)
+{
+	t_list	*current;
+	t_list	*next_node;
+
+	current = lst;
+	while (current)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+}
+
+int	main(void)
+{
+	// create linked list: 4 -> 2 -> 5 -> 1 -> 3
+	t_list	*head;
+	head = create_node(4);
+	head->next = create_node(2);
+	head->next->next = create_node(5);
+	head->next->next->next = create_node(1);
+	head->next->next->next->next = create_node(3);
+
+	printf("Original list: ");
+	print_list(head);
+	printf("\n");
+
+	sort_list(head, &ascending);
+	printf("Sorted list: ");
+	print_list(head);
+	printf("\n");
+
+	free_list(head);
+	return (0);
+}
+*/
